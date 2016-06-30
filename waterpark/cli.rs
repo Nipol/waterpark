cd5bd701c7adcffe1352bbf902d47fee8e3c30ad
Nipol <ys.choi@me.com>
2016-06-30 23:28:19 +0900

    re org cli options

--- waterpark/cli.rs
+++ waterpark/cli.rs
@@ -1,17 +1,36 @@
 pub const USAGE: &'static str = "
-Waterpark File share platform.
+Waterpark : Simply File share platform.
+  By Nipol
 
 Usage:
-  waterpark
-  waterpark init
-  waterpark config <path>
+  waterpark [Options]
+  waterpark init [Options]
   waterpark help
+  waterpark version
+
+Default Options:
+  -c --config PATH         서버 설정을 담은 config.json 파일 위치를 지정합니다.
+                           [Default: ./config.json]
+
+init Options:
+  -n --name SERVERNAME     서버 이름을 지정합니다. 
+                           [Default: waterpark].
+  -p --port PORT           서버가 오픈될 포트를 지정합니다.
+                           [Default: 80].
+  --tls                    TODO
 ";
 
 #[derive(Debug, RustcDecodable)]
 pub struct Args {
   pub cmd_init: bool,
-  pub cmd_config: bool,
   pub cmd_help: bool,
-  pub arg_path: Vec<String>,
+  pub cmd_version: bool,
+  pub flag_config: Vec<String>,
+  pub flag_name: String,
+  pub flag_port: u16,
+  pub flag_tls: bool,
+}
+
+pub fn show_version() {
+  println!("0.0.1");
 }
--- waterpark/configuration.rs
+++ waterpark/configuration.rs
@@ -1,4 +1,5 @@
 use cli::{USAGE, Args};
+use rustc_serialize::json::{self, ToJson, Json};
 use docopt::Docopt;
 
 pub struct Configuration {
@@ -6,9 +7,26 @@ pub struct Configuration {
 }
 
 impl Configuration {
-    pub fn parse() -> Self {
-      Configuration {
-        args: Docopt::new(USAGE).and_then(|d| d.decode()).unwrap_or_else(|e| e.exit()),
-      }
+  pub fn parse() -> Self {
+    Configuration {
+      args: Docopt::new(USAGE).and_then(|d| d.decode()).unwrap_or_else(|e| e.exit()),
     }
+  }
+}
+
+#[derive(RustcEncodable)]
+pub struct WaterparkConfig {
+  name: String,
+  port: u16,
+  tls: bool,
+}
+
+impl WaterparkConfig {
+  pub fn init() -> Self {
+    WaterparkConfig {
+      name: "Waterpakr".to_string(),
+      port: 80,
+      tls: false,
+    }
+  }
 }
